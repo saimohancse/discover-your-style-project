98b36448b8a66486aff6edcc17b6d7420c1e03c2
saimohancse <96363503+saimohancse@users.noreply.github.com>
2023-01-31 22:30:56 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -8,6 +8,25 @@ for (let i = 0; i < buttons.length; i++) {
       price: buttons[i].previousElementSibling.textContent
     };
     cart.push(product);
-    console.log(cart);
+    updateCartDisplay();
   });
 }
+
+function updateCartDisplay() {
+  const cartContainer = document.querySelector(".cart-container");
+  cartContainer.innerHTML = "";
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const product = cart[i];
+    total += parseFloat(product.price.replace("$", ""));
+    const productElement = document.createElement("div");
+    productElement.innerHTML = `
+      <p>${product.name} - ${product.price}</p>
+    `;
+    cartContainer.appendChild(productElement);
+  }
+  const totalElement = document.createElement("p");
+  totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+  cartContainer.appendChild(totalElement);
+}
+
